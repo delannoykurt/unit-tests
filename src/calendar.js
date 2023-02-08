b6ffb1c554af94406c64bffc30a3c1c7dc0cb4cb
kurt delannoy <kurtdelannoy@cerise-2.local>
2023-02-09 00:31:06 +0100

    docs(common): add feature manage the calendar, add, remove and get dates

--- src/calendar.js
+++ src/calendar.js
@@ -1,6 +1,28 @@
 // this is a function represented the calendar 
 // objectif: to have the current progression on the unit tests
 
-const calendar = (date) => {
-	return 0;
+const calendar = () => {
+	return 0; 
 }
+
+
+
+let dates = [];
+
+const addDate = (date) => {
+	if(date === undefined){
+		return 0;
+	}
+	dates.push(date);
+};
+
+const removeDate = (date) => {
+	dates = dates.filter(d => d.getTime() !== date.getTime());
+};
+
+const getDates = () => {
+	return dates.slice();
+};
+
+
+module.exports = { addDate, removeDate, getDates, calendar };
--- test/calendar.spec.js
+++ test/calendar.spec.js
@@ -4,11 +4,42 @@
 const assert = require('assert');
 const { expect } = require('chai');
 const { calendar } = require('../src/calendar.js');
-
+const { addDate, removeDate, getDates } = require('../src/calendar.js');
 
 describe('this is a calendar function', () => {
 	it('should be add the new date in the calendar', () => {
-		const calendar = { year, month, day };
-		expect().be.to.eq(0);
+		let result;
+		result = calendar();
+		expect(result).be.to.eq(0);
+	});
+});
+
+
+
+describe('Calendar functions', () => {
+	it('Should add a date to the calendar', () => {
+		const date = new Date();
+		addDate(date);
+		const dates = getDates();
+		expect(dates).to.include(date);
+	});
+
+	it('Should remove a date from the calendar', () => {
+		const date = new Date();
+		addDate(date);
+		removeDate(date);
+		const dates = getDates();
+		expect(dates).to.not.include(date);
+	});
+
+	it('Should get all dates from the calendar', () => {
+		const date1 = new Date();
+		const date2 = new Date();
+		addDate(date1);
+		addDate(date2);
+		const dates = getDates();
+		expect(dates).to.have.lengthOf(2);
+		expect(dates).to.include(date1);
+		expect(dates).to.include(date2);
 	});
 });
