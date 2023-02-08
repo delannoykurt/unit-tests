3078750a9d06b59b0d41fae65ec2de33a2731beb
kurt delannoy <kurtdelannoy@cerise-2.local>
2023-02-08 15:15:08 +0100

    docs(common): initial commmit

---
+++ src/calendar.js
@@ -0,0 +1,6 @@
+// this is a function represented the calendar 
+// objectif: to have the current progression on the unit tests
+
+const calendar = (date) => {
+	return 0;
+}
---
+++ test/calendar.spec.js
@@ -0,0 +1,14 @@
+// this is a function represented the calendar 
+// objectif: to have the current progression on the unit tests
+
+const assert = require('assert');
+const { expect } = require('chai');
+const { calendar } = require('../src/calendar.js');
+
+
+describe('this is a calendar function', () => {
+	it('should be add the new date in the calendar', () => {
+		const calendar = { year, month, day };
+		expect().be.to.eq(0);
+	});
+});
